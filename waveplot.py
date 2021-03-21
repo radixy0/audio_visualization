8f8f7a6070ab74b413d868623f9ec3235fe0305f
Lukas Ernst <ernst.lukas@gmx.net>
2021-03-21 17:01:04 +0100

    finished waveplot.py

--- waveplot.py
+++ waveplot.py
@@ -1,21 +1,22 @@
 import librosa
 import librosa.display
 import argparse
+import matplotlib.pyplot as plt
 
 class Options:
     """
     A class to manage command line options
     """
 
-    infile = None           # file list that we will read looking for entries
+    infile = ""         # file list that we will read looking for entries
     
-    image = 'output.png'  # image name output
+    image = 'waveplot.png'  # image name output
 
     def __init__(self):
         """
         Inits the class
         """
-        self.infile = None
+        self.infile = ""
         
         self._get_command_line_arguments()
 
@@ -31,8 +32,8 @@ class Options:
 
         parser = argparse.ArgumentParser(description='Script to analyse flexlm log files.')
 
-        parser.add_argument('-i', '--image', action='store', dest='image', help='Image output name, defaults to image.png', default='image.png')
-        parser.add_argument('files', metavar='Files', type=str, nargs='+', help='Input audio file')
+        parser.add_argument('-i', '--image', action='store', dest='image', help='Image output name, defaults to waveplot.png', default='waveplot.png')
+        parser.add_argument('files', metavar='Files', type=str, help='Input audio file')
 
         self.options = parser.parse_args()
 
@@ -48,3 +49,4 @@ y, sr = librosa.load(filename)
 # trim silent edges
 whale_song, _ = librosa.effects.trim(y)
 librosa.display.waveplot(whale_song, sr=sr)
+plt.savefig(Options.image)
